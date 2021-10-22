f7171c16004ffb8447efd5d10af6eb9de6b35280
Lisa <wiegandtlisa@gmail.com>
2021-10-22 17:20:27 +0200

    fixed javascript forwarding

--- JS/login.js
+++ JS/login.js
@@ -1,10 +1,8 @@
-function submit_login() {
+function login() {
     if(document.getElementById("fname").value === "Lisa-Marlen" && document.getElementById("lname").value === "Wiegandt"
     && document.getElementById("password").value === "1234"){
-
-        //window.location.replace("")
         alert("Welcome!");
-        window.location.href = "../HTML/home.html";
+        document.location.href = "../HTML/home.html", true;
     }else {
         alert("Wrong credentials - Try again!");
     }
