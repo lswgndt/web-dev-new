d3344739489a41ecb91f6cc942f4db8be3a3229c
Lisa <wiegandtlisa@gmail.com>
2021-10-29 16:30:15 +0200

    added basic structure for admin and staff view

--- JS/login.js
+++ JS/login.js
@@ -2,7 +2,7 @@ function login() {
     if(document.getElementById("fname").value === "Lisa-Marlen" && document.getElementById("lname").value === "Wiegandt"
     && document.getElementById("password").value === "1234"){
         alert("Welcome!");
-        document.location.href = "../HTML/home.html", true;
+        document.location.href = "../HTML/admin-home.html", true;
     }else {
         alert("Wrong credentials - Try again!");
     }
