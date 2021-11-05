04851abb5adaccea0dab6647fc3b51747ee4e5e1
Lisa <wiegandtlisa@gmail.com>
2021-11-05 18:11:39 +0100

    changed admin related files to fit assignment

--- JS/admin.js
+++ JS/admin.js
@@ -9,10 +9,8 @@ function User(username, password){
 
 function admin_login() {
     if(current_tries_admin < max_tries){
-        console.log("TEST")
         if(admin.username === document.getElementById("admin_username").value
             && admin.password === document.getElementById("admin_password").value ) {
-            console.log("TEST2")
             document.location.href = "../HTML/admin-home.html", true;
         }else {
             current_tries_admin = current_tries_admin + 1;
@@ -23,3 +21,58 @@ function admin_login() {
         disable_log_ins();
     }
 }
+
+function hide_admin_lists(){
+    document.getElementById("studentContainer").style.display ="none";
+    document.getElementById("staffContainer").style.display ="none";
+    document.getElementById("addStudent").style.display ="none";
+    document.getElementById("addStaff").style.display ="none";
+}
+
+function show_students() {
+    let x = document.getElementById("studentContainer");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function add_student_form(){
+    let x = document.getElementById("addStudent");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function add_student(){}
+
+function update_student(){}
+
+function delete_student(){}
+
+function show_staff() {
+    let x = document.getElementById("staffContainer");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function add_staff_form(){
+    let x = document.getElementById("addStaff");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function add_staff(){}
+
+function update_staff(){}
+
+function delete_staff(){}
